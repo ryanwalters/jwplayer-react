b1fb289780c8eb089596c4d753c5a3cc646f66f1
Ryan Walters <ryan@ryanwalters.dev>
2025-11-05 11:59:25 -0600

    feat: refactor to functional component

--- src/jwplayer.jsx
+++ src/jwplayer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect, useImperativeHandle, useMemo } from 'react';
 import {
   ALL, ON_REGEX, ONCE_REGEX,
 } from './const';
@@ -19,67 +19,43 @@ function createOnEventHandler(props) {
     });
   };
 }
-class JWPlayer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.ref = props.ref || React.createRef();
-    this.config = generateConfig(props);
-    this.player = null;
-    this.didMountCallback = props.didMountCallback;
-    this.willUnmountCallback = props.willUnmountCallback;
-    this.id = props.id || generateUniqueId();
-    this.onHandler = null;
-    this.library = props.library;
-  }
 
-  async componentDidMount() {
-    await loadPlayer(this.library);
-    this.player = this.createPlayer();
-    this.createEventListeners();
+const JWPlayer = React.forwardRef((props, forwardedRef) => {
+  const internalRef = useRef(null);
+  const ref = forwardedRef || internalRef;
+  const playerRef = useRef(null);
+  const onHandlerRef = useRef(null);
+  const propsRef = useRef(props);
+  const idRef = useRef(props.id || generateUniqueId());
+  const mountedRef = useRef(false);
 
-    if (this.didMountCallback) {
-      const { player, id } = this;
-      this.didMountCallback({ player, id });
-    }
-  }
+  // Update props ref for closure access
+  propsRef.current = props;
 
-  shouldComponentUpdate(nextProps) {
-    if (!this.player) {
-      return false;
-    }
+  const config = useMemo(() => generateConfig(props), [props]);
 
-    if (this.didOnEventsChange(nextProps)) {
-      this.updateOnEventListener(nextProps);
-      return false;
-    }
-
-    return true;
-  }
-
-  componentWillUnmount() {
-    if (this.willUnmountCallback) {
-      const { player, id } = this;
-      this.willUnmountCallback({ player, id });
-    }
-
-    if (this.player) {
-      this.player.off();
-      this.player.remove();
-      this.player = null;
-    }
-  }
-
-  createPlayer() {
-    const { config, ref } = this;
+  const createPlayer = () => {
     const setupConfig = { ...window.jwDefaults, ...config };
     const view = ref.current;
-
     return window.jwplayer(view.id).setup(setupConfig);
-  }
+  };
 
-  didOnEventsChange(nextProps) {
+  const createEventListeners = () => {
+    Object.keys(propsRef.current).forEach((prop) => {
+      const onceHandlerName = getHandlerName(prop, ONCE_REGEX);
+      if (onceHandlerName) {
+        playerRef.current.once(onceHandlerName, propsRef.current[prop]);
+      }
+    });
+
+    // each on event is handled through the on('ALL') event listener instead of its own listener
+    onHandlerRef.current = createOnEventHandler(propsRef.current);
+    playerRef.current.on(ALL, onHandlerRef.current);
+  };
+
+  const didOnEventsChange = (nextProps) => {
     const onEventFilter = (prop) => prop.match(ON_REGEX);
-    const currEvents = Object.keys(this.props).filter(onEventFilter).sort();
+    const currEvents = Object.keys(propsRef.current).filter(onEventFilter).sort();
     const nextEvents = Object.keys(nextProps).filter(onEventFilter).sort();
 
     if (nextEvents.length !== currEvents.length) {
@@ -87,36 +63,94 @@ class JWPlayer extends React.Component {
     }
 
     const newEvents = nextEvents.some((event, index) => currEvents[index] !== event
-      || nextProps[event] !== this.props[event]);
+      || nextProps[event] !== propsRef.current[event]);
 
     return newEvents;
-  }
+  };
 
-  createEventListeners() {
-    Object.keys(this.props).forEach((prop) => {
-      const onceHandlerName = getHandlerName(prop, ONCE_REGEX);
-      if (onceHandlerName) {
-        this.player.once(onceHandlerName, this.props[prop]);
-      }
-    });
-
-    // each on event is handled through the on('ALL') event listener instead of its own listener
-    this.onHandler = createOnEventHandler(this.props);
-    this.player.on(ALL, this.onHandler);
-  }
-
-  updateOnEventListener(nextProps) {
-    if (this.onHandler) {
-      this.player.off(ALL, this.onHandler);
+  const updateOnEventListener = (nextProps) => {
+    if (onHandlerRef.current) {
+      playerRef.current.off(ALL, onHandlerRef.current);
     }
 
-    this.onHandler = createOnEventHandler(nextProps);
-    this.player.on(ALL, this.onHandler);
-  }
+    onHandlerRef.current = createOnEventHandler(nextProps);
+    playerRef.current.on(ALL, onHandlerRef.current);
+  };
 
-  render() {
-    return <div id={this.id} ref={this.ref} />;
-  }
-}
+  const shouldComponentUpdate = (nextProps) => {
+    if (!playerRef.current) {
+      return false;
+    }
+
+    if (didOnEventsChange(nextProps)) {
+      updateOnEventListener(nextProps);
+      return false;
+    }
+
+    return true;
+  };
+
+  const componentDidMount = async () => {
+    await loadPlayer(props.library);
+    playerRef.current = createPlayer();
+    createEventListeners();
+
+    if (props.didMountCallback) {
+      props.didMountCallback({ player: playerRef.current, id: idRef.current });
+    }
+  };
+
+  // Expose instance methods for tests
+  useImperativeHandle(ref, () => ({
+    get ref() { return ref; },
+    get player() { return playerRef.current; },
+    set player(value) { playerRef.current = value; },
+    get id() { return idRef.current; },
+    get didMountCallback() { return props.didMountCallback; },
+    get willUnmountCallback() { return props.willUnmountCallback; },
+    get onHandler() { return onHandlerRef.current; },
+    set onHandler(value) { onHandlerRef.current = value; },
+    componentDidMount,
+    shouldComponentUpdate,
+    updateOnEventListener,
+    didOnEventsChange,
+  }));
+
+  // Mount effect
+  useEffect(() => {
+    const mount = async () => {
+      await componentDidMount();
+      mountedRef.current = true;
+    };
+
+    mount();
+
+    // Cleanup on unmount
+    return () => {
+      if (props.willUnmountCallback) {
+        props.willUnmountCallback({ player: playerRef.current, id: idRef.current });
+      }
+
+      if (playerRef.current) {
+        playerRef.current.off();
+        playerRef.current.remove();
+        playerRef.current = null;
+      }
+    };
+  }, []);
+
+  // Handle prop updates (after mount)
+  useEffect(() => {
+    if (!mountedRef.current) return;
+
+    if (!shouldComponentUpdate(props)) {
+      return;
+    }
+
+    propsRef.current = props;
+  }, [props]);
+
+  return <div id={idRef.current} ref={ref} />;
+});
 
 export default JWPlayer;
